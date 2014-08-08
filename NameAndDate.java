706bfa7714861f8ed5270759eb944294a4c9e233
jagadishchandran <jagadish.chandran@gmail.com>
2014-08-08 14:19:41 +0530

    Create NameAndDate.java
    initial checkin

---
+++ NameAndDate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/** NameAndDate prints my name and the exact date and time. */
+public class NameAndDate
+{ public static void main(String[] args)
+{ System.out.print("Fred Mertz --- ");
+// The next statement creates an object:
+GregorianCalendar c = new GregorianCalendar();
+System.out.println(c.getTime()); // ask c the time and print its reply
+System.out.println();
+System.out.println("Finished");
+}
+}
